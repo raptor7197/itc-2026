ab88e4286529927e459ece2caa1a0d823568b725
raptor7197 <vamsikrishna75836@gmail.com>
2026-03-17 00:49:43 +0530

    fix: Marquee Update CFP date

--- itc_2026/src/Pages/Home.jsx
+++ itc_2026/src/Pages/Home.jsx
@@ -118,12 +118,12 @@ export default function Home() {
         <div className="relative w-full">
           <Marquee className="h-[8vw] md:h-[10vw] lg:h-[8vw]">
             <div className="mx-5 text-blue-300">
-              Key Timeline : Call for tutorial -
+              Key Timeline : Call for Paper - 31st March, Call for tutorial -
               31st March
             </div>
             <div className="mx-5">•</div>
             <div className="mx-5 text-blue-300">
-              Key Timeline : Call for tutorial -
+              Key Timeline : Call for Paper - 31st March, Call for tutorial -
               31st March
             </div>
             <div className="mx-5">•</div>
