17e580bab6d9cbb32a4df9d06a6d96f032c50fee
raptor7197 <vamsikrishna75836@gmail.com>
2026-03-29 00:27:21 +0530

    remove program in header

--- itc_2026/src/Pages/Components/Header.jsx
+++ itc_2026/src/Pages/Components/Header.jsx
@@ -265,29 +265,6 @@ export default function Example() {
 
                   <Disclosure as="div" className="-mx-3">
                     <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base/7 font-semibold text-white text-xl hover:text-blue-500">
-                      Program
-                      <ChevronDownIcon
-                        aria-hidden="true"
-                        className="size-5 flex-none group-data-[open]:rotate-180"
-                      />
-                    </DisclosureButton>
-                    <DisclosurePanel className="mt-2 space-y-2">
-                      {[...ProgramList].map((item) => (
-                        <DisclosureButton
-                          key={item.name}
-                          as={Link}
-                          to={item.href}
-                          className="block rounded-lg py-2 pl-6 pr-3 text-sm/7 font-semibold text-white hover:text-blue-500"
-                          onClick={() => setMobileMenuOpen(false)}
-                        >
-                          {item.name}
-                        </DisclosureButton>
-                      ))}
-                    </DisclosurePanel>
-                  </Disclosure>
-
-                  <Disclosure as="div" className="-mx-3">
-                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base/7 font-semibold text-white text-xl hover:text-blue-500">
                       Authors
                       <ChevronDownIcon
                         aria-hidden="true"
